bbacd19f8b8816c965f10fcba5130009d9b44b99
lama23 <florenz.erstling@lalalama.de>
2016-12-16 17:20:22 +0100

    Initial Commit

---
+++ SangooAlpha-all/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SangooAlpha-all
+//
+//  Created by Florenz Erstling on 16.12.16.
+//  Copyright © 2016 Florenz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
